4b431829d92673174f54155b3a071552952ac787
xup6890609 <71679918+xup6890609@users.noreply.github.com>
2020-11-03 16:03:08 +0800

    建立碰撞偵測器

--- Unity_Asia_2DGame/Assets/Scripts/MenuManager.cs
+++ Unity_Asia_2DGame/Assets/Scripts/MenuManager.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
-    void StartGame()
-    { }
-    void LeaveGame() 
-    { }
+public void StartGame()
+    {
+        print("開始遊戲");
+        SceneManager.LoadScene("關卡1");
+    }
+    public void LeaveGame()
+    {
+        print("離開遊戲");
+        Application.Quit();
+
+    }
 }
