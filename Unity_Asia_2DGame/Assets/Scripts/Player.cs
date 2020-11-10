6a526d0f90bd2edbe2749dd8c3d4fd125d0734b0
xup6890609 <71679918+xup6890609@users.noreply.github.com>
2020-11-10 16:07:36 +0800

    新增追蹤攝影機、讓鑽石消失

--- Unity_Asia_2DGame/Assets/Scripts/Player.cs
+++ Unity_Asia_2DGame/Assets/Scripts/Player.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public GameObject final;
+
+    [Header("鑽石數量")]
+    public Text textcount;
+
+    public int count;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         print("碰到傳送門惹");
@@ -11,7 +17,12 @@ public class Player : MonoBehaviour
             final.SetActive(true);
 
         }
-        
+        if (collision.tag=="鑽石")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textcount.text = "鑽石數量:" + count;
+        }
     }
 
 }
